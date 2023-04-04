2a9fbd18c9467b99a1e984fed972318abed71192
Quaya Brown <technoquaya@gmail.com>
2023-04-04 09:38:30 -0400

    inital commit

---
+++ NBA-dream/script.js
@@ -0,0 +1,85 @@
+let player1 = {
+
+    name : "Kobe Bryant",
+    position : 16,
+    height : "6'2",
+    stats :
+    rings:
+
+    eat : function (){
+        console.log("Stone is eating nachos")
+    },
+    drink : function (){
+        console.log("Stone is drinking gatorade")
+    },
+    play : function(){
+        console.log("Stone is playing basketball");
+    }
+};
+
+let player2 = {
+
+    name : "Michael Jordan",
+    position : 16,
+    height : "6'2",
+    stats :
+    rings:
+
+    eat : function (){
+        console.log("Stone is eating nachos")
+    },
+    drink : function (){
+        console.log("Stone is drinking gatorade")
+    },
+    play : function(){
+        console.log("Stone is playing basketball");
+    }
+};
+let human1 = {
+
+    name : "Stone",
+    age : 16,
+    height : "6'2",
+
+    eat : function (){
+        console.log("Stone is eating nachos")
+    },
+    drink : function (){
+        console.log("Stone is drinking gatorade")
+    },
+    play : function(){
+        console.log("Stone is playing basketball");
+    }
+};
+let human1 = {
+
+    name : "Stone",
+    age : 16,
+    height : "6'2",
+
+    eat : function (){
+        console.log("Stone is eating nachos")
+    },
+    drink : function (){
+        console.log("Stone is drinking gatorade")
+    },
+    play : function(){
+        console.log("Stone is playing basketball");
+    }
+};
+let human1 = {
+
+    name : "Stone",
+    age : 16,
+    height : "6'2",
+
+    eat : function (){
+        console.log("Stone is eating nachos")
+    },
+    drink : function (){
+        console.log("Stone is drinking gatorade")
+    },
+    play : function(){
+        console.log("Stone is playing basketball");
+    }
+};
